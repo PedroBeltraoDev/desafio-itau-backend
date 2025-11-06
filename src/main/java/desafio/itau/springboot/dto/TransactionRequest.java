1e2b3759527a6f1cc9d561102a1bf22901092d5d
PedroBeltraoDev <pedrobeltraodev@gmail.com>
2025-11-05 23:56:52 -0300

    commit one

---
+++ src/main/java/desafio/itau/springboot/dto/TransactionRequest.java
@@ -0,0 +1,21 @@
+package desafio.itau.springboot.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.OffsetDateTime;
+
+public class TransactionRequest {
+
+    @NotNull
+    private double valor;
+
+    @NotNull
+    public OffsetDateTime dataHora;
+
+    public double getValor() {
+        return valor;
+    }
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+}
